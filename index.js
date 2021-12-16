585646ece9f26ec5955be418d962d1d932c29f7c
C0mbed <matt@farncombe.co>
2021-12-16 05:24:10 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // your code here
+
+function shout(inString) {
+    return inString.toUpperCase();
+}
+
+function whisper(inString) {
+    return inString.toLowerCase();
+}
+
+function logShout(inString) {
+    console.log(inString.toUpperCase());
+}
+
+function logWhisper(inString) {
+    console.log(inString.toLowerCase());
+}
+
+function sayHiToHeadphonedRoomate(inString) {
+    if (inString == inString.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (inString == inString.toUpperCase()) {
+        return "YES INDEED!";
+    } else {
+        return "I would love to!";
+    }
+}
